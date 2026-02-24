e7bf117775ab549a7dff95719d5664b4b6a193db
Veeksha29 <veeksha2902@gmail.com>
2026-02-24 22:21:51 +0530

    google map add

--- src/api/menuApi.js
+++ src/api/menuApi.js
@@ -1,11 +1,11 @@
 import http from "./http";
 
 const MENU_API = "/menu";
-
+const ADDMENU_API = "/menu-service";
 export const menuApi = {
   // ✅ ADD MENU (FIXED)
   add: async (payload) => {
-    const { data } = await http.post(MENU_API, payload, {
+    const { data } = await http.post(ADDMENU_API, payload, {
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
